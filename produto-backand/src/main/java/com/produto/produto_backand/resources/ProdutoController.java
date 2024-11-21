e8387c8fd8a507308232d3b73d6b122cbc21b504
carlosjc31 <carlosjc31@hotmail.com>
2024-11-21 16:20:06 -0300

    criando uma lista de produtos

--- produto-backand/src/main/java/com/produto/produto_backand/resources/ProdutoController.java
+++ produto-backand/src/main/java/com/produto/produto_backand/resources/ProdutoController.java
@@ -1,5 +1,8 @@
 package com.produto.produto_backand.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,7 @@ import com.produto.produto_backand.modelos.Produto;
 @RestController
 public class ProdutoController {
     
-    @GetMapping("produtos")
+    @GetMapping("produto")
     public Produto getProduto(){
 
         Produto p = new Produto();
@@ -18,4 +21,36 @@ public class ProdutoController {
 
         return p;
     }
+    @GetMapping("produtos")
+    public List<Produto> getProdutos(){
+        Produto p1 = new Produto();
+        p1.setId(1);
+        p1.setNome("Notebokk Dell");
+        p1.setPreco(2000.00);
+
+        Produto p2 = new Produto();
+        p2.setId(2);
+        p2.setNome("Notebokk Asus");
+        p2.setPreco(2000.00);
+
+        Produto p3 = new Produto();
+        p3.setId(3);
+        p3.setNome("Notebokk Lenovo");
+        p3.setPreco(2000.00);
+
+        Produto p4 = new Produto();
+        p4.setId(4);
+        p4.setNome("Notebokk Apple");
+        p4.setPreco(4000.00);
+
+        List<Produto> ListaProdutos = new ArrayList<>();
+
+        ListaProdutos.add(p1);
+        ListaProdutos.add(p2);
+        ListaProdutos.add(p3);
+        ListaProdutos.add(p4);
+
+        return ListaProdutos;
+    }
+
 }
